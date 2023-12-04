5e6f0d2477fbd65648f5e84a33208973bb1fcd5b
RoaringDragon <98014574+RoaringDragon@users.noreply.github.com>
2023-12-04 16:38:23 +0100

    Worked some one loading training data

--- Scripts/stateDetection.py
+++ Scripts/stateDetection.py
@@ -2,13 +2,39 @@ import os
 import numpy as np
 import pandas as pd
 from joblib import dump, load
+from feat import Detector
 
-def loadDictionaryData(img_path)
+def loadTrainingData(data_path):
     # Function for loading a dictinary of images as inpuit data (Used for training)
-     images = os.listdir(img_path)
+    categories = os.listdir(data_path)
 
-def train_models(data)
+    labels = []
+    aus_data = []
+
+    counter = 0
+
+    for categorie in categories:
+        img_path = os.path.join(data_path, categorie)
+        images = os.listdir(img_path)
+        for file in images:
+            if counter == 1: # Remove when testing done
+                break
+            file_path = os.path.join(img_path,file)
+            # replace with detector
+
+            detector = Detector(device="cuda")
+            data = detector.detect_image(file_path)
+
+            labels.append(categorie)
+            aus_data.append(data.aus)
+
+            counter = 1
+
+    return labels, aus_data
+
+def train_models(data):
     # Function for training modules on train data
+    ...
 
 def use_model(data, modelPath):
     # Function for using premade model to classify AUS data
@@ -19,7 +45,8 @@ def main():
     path = os. getcwd()
 
     #Training
-    img_path = os.path.join(path,"Data")
+    labels, data = loadTrainingData(os.path.join(path,"Data","DiffusionFER","DiffusionEmotion_S","cropped"))
+    print(data)
 
     #aus = pd.read_csv(os.path.join(path,"processed","aus.csv")) # Should be real time for final version
 
